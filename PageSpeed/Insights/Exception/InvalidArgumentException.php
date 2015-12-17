e2f70e4cdae147ea7f7207c621a1e34609c41c75
bhowe <howe.bobby@gmail.com>
2015-12-16 21:32:35 -0600

    planning

--- PageSpeed/Insights/Exception/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace PageSpeed\Insights\Exception;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
-$pageSpeed = new \PageSpeed\Insights\Service();
-$pageSpeed->getResults('http://www.example.com');
+//http://4evertutorials.blogspot.com/2014/09/google-pagespeed-insights-api-php.html
+//https://developers.google.com/speed/docs/insights/v1/getting_started?hl=en
+
+// read in json files and build a url list of the type sites in the world
+// use curl to read the page speed for each url and average it
+// Track stats for images, JS, CSS HTML from each site to display in pie chart
+// track page speed suggestsions such as leverage browser caching, minify JS and defering js and display pie chart
